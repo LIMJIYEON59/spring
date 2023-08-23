3477bf223226394b3f6c22e47bba3dd922cf97b4
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-23 20:37:59 +0900

    vo table 다시 생성함

--- src/main/java/kh/lcalss/db1/member/model/vo/MemberVo.java
+++ src/main/java/kh/lcalss/db1/member/model/vo/MemberVo.java
@@ -4,42 +4,53 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MemberVo {
-/*	
-MEMBER_ID      VARCHAR2(20) 
-MEMBER_PWD     VARCHAR2(20) 
-MEMBER_NAME    VARCHAR2(20) 
-
-	"MID"	VARCHAR2(20)		NOT NULL,
-	"MPWD"	VARCHAR2(20)		NOT NULL,
-	"MNAME"	VARCHAR2(50)		NOT NULL,
-	"MEMAIL"	VARCHAR2(150)		NOT NULL
-*/
-	private String memberId;
-	private String memberPwd;
-	private String memberName;
+//	MEMBER------ -------- ------------- 
+//	MID    NOT NULL VARCHAR2(20)  
+//	MPWD   NOT NULL VARCHAR2(20)  
+//	MNAME  NOT NULL VARCHAR2(50)  
+//	MEMAIL NOT NULL VARCHAR2(150)
+	
+	private String mid;
+	private String mpwd;
+	private String mname;
+	private String memail;
+	
 	@Override
 	public String toString() {
-		return "MemberVo [memberId=" + memberId + ", memberPwd=" + memberPwd + ", memberName=" + memberName + "]";
-	}
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public String getMemberPwd() {
-		return memberPwd;
-	}
-	public void setMemberPwd(String memberPwd) {
-		this.memberPwd = memberPwd;
-	}
-	public String getMemberName() {
-		return memberName;
-	}
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
+		return "MemberVo [mid=" + mid + ", mpwd=" + mpwd + ", mname=" + mname + ", memail=" + memail + "]";
 	}
 
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public String getMpwd() {
+		return mpwd;
+	}
+
+	public void setMpwd(String mpwd) {
+		this.mpwd = mpwd;
+	}
+
+	public String getMname() {
+		return mname;
+	}
+
+	public void setMname(String mname) {
+		this.mname = mname;
+	}
+
+	public String getMemail() {
+		return memail;
+	}
+
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
 	
 	
 }
