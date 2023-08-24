d8c082b3554827541704237d7058fff6c9fdb9c5
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-25 01:18:38 +0900

    Spring member 수업 복습

--- 20230823_spring_db_01/src/main/java/kh/lcalss/db1/member/model/dao/MemberDao.java
+++ 20230823_spring_db_01/src/main/java/kh/lcalss/db1/member/model/dao/MemberDao.java
@@ -11,24 +11,38 @@ import kh.lcalss.db1.member.model.vo.MemberVo;
 @Repository("memberDao")
 public class MemberDao {
 	
-	// Autowired 사용시 Spring에서 이미 만들어진 빈객체를 가져다 씀
+	// Autowired 사용시 Spring에서 이미 만들어진 빈객체를 가져다 씀(Spring에서 'SqlSession' 빈을 자동으로 주입한다.)
 	//따라서 =null 또는 = new Xxx() 대입하면 안됨!!
 	@Autowired
 	private SqlSession sqlSession;
 	
+	// 모든 회원 정보를 DB에서 검색하여 리스트로 반환한다. MyBatis의 'selectList' 메서드를 사용하여 SQL 쿼리를 실행한다.
 	public List<MemberVo> selectList() {
 		return sqlSession.selectList("member.selectList");
 	}
+	// 주어진 'pk'에(Primary Key) 해당하는 회원 정보를 DB에서 검색하여 반환한다. MyBatis의 'selectOne' 메서드를 사용하여 SQL쿼리를 실행한다.
 	public MemberVo selectOne(String pk) {
 		return sqlSession.selectOne("member.selectOne", pk);
 	}
+	// 주어진 'MemberVo' 객체를 DB에 삽입한다. MyBatis의 'insert' 메서드를 사용하여 SQL 쿼리를 실행한다.
 	public int insert(MemberVo vo) {
 		return sqlSession.insert("member.insert", vo);
 	}
+	// 회원정보 업데이트, Mybatis의 'update'메서드를 사용하여 SQL 쿼리를 실행한다.
 	public int update(MemberVo vo) {
 		return sqlSession.update("member.update", vo);
 	}
+	
+	//String 타입의 pk 파라미터를 받는다. 반환타입은 int이다. (pk는 주로 회원의 Primary Key 값을 나타낸다) 이 메서드는 특정 회원을 DB에서 삭제하고 삭제된 레코드의 개수를 반환한다.
+	//sqlSession 객체를 사용하여 DB작업수행 
+	//'sqlSession.delete() 메서드를 호출하여 DB에서 SQL 쿼리를 실행하고 그 결과로 삭제된 레코드의 개수를 반환한다.
+	//"member.delete"는 실행할 SQL 쿼리의 이름이다.(MyBatis XML 파일에 정의된 SQL 맵퍼의 쿼리 ID이다.)-> "member.delete"는 회원을 삭제하는 SQL 쿼리를 실행하라는 의미이다. (MyBatis XML 설정 파일에서 해당 쿼리를 찾아서 실행하라는 역할이다)
+	//'pk'는 SQL 쿼리에 전달될 파라미터로 사용된다 SQL 쿼리 내에서 해당 파라미터를 이용해 어떤 회원을 삭제할지 결정한다.
+	//결론:'pk'를 사용하여 DB에서 해당 회원을 삭제하고 삭제된 레코드의 개수를 반환한다.
 	public int delete(String pk) {
-		return sqlSession.delete("member.delete", pk);
+		return sqlSession.delete("member.delete", pk); //MyBatis의 sqlSession.delete()메서드
 	}
 }
+
+// 이 'MemberDao' 클래스는 Spring Framework와 MyBatis를 함께 사용하여 DB와 상호작용을 처리한다.
+// DAO 클래스를 Spring loC컨테이너에 빈으로 등록하여 다른 컴포넌틍서 사용할 수 있게 된다.
--- 20230823_spring_db_01/src/main/java/kh/lcalss/db1/member/model/service/MemberService.java
+++ 20230823_spring_db_01/src/main/java/kh/lcalss/db1/member/model/service/MemberService.java
@@ -12,26 +12,47 @@ import kh.lcalss.db1.member.model.vo.MemberVo;
 @Service
 public class MemberService {
 	
+	//이 어노테이션을 사용해서 Spring에서 'MemberDao' 빈을 자동으로 주입한다.
 	@Autowired
 	private MemberDao memberDao;
 	
+	// 모든 회원 정보를 DB에서 검색하여 리스트로 반환한다. service의 selectList()메서드가 memberDao의 selectList()를 호출해서 DB의 모든 회원 정보를 검색한다.
+	//DB에서 모든 회원 정보를 검색하는 쿼리 실행 
+	//DB로부터 검색된 회원 정보는 'List<MemberVo>'형태로 반환된다.
+	//'MemberService' 클래스의 'selectList()'메서드는 DB로부터 검색된 회원 목록을 받아와 클라이언트 코드(또는 다른 서비스)에게 리스트로 반환한다.
+	//결론: 서비스의 메서드를 이용해 dao의 메서드를 호출해 회원목록을 가져온다!!
 	public List<MemberVo> selectList(){
-		return memberDao.selectList();
+		return memberDao.selectList();	
 	}
+	
+	//'pk'에 해당하는 회원 정보를 가져와서 반환한다.
 	public MemberVo selectOne(String pk) {
 		return memberDao.selectOne(pk);
 	}
+	//주어진 'MemberVo' 객체를 DB에 삽입한다. 회원정보를 추가한다.
 	public int insert(MemberVo vo) {
 		return memberDao.insert(vo);
 	}
+	//회원정보를 업데이트하고 새로운 정보를 삽입한다. 먼저 'update()'메서드를 호출하여 회원 정보를 업데이트하고 
+	//'insert()' 메서드를 호출하여 새로운 정보를 추가한다.
 	public int insertReply(MemberVo vo) {
 		memberDao.update(vo);
 		return memberDao.insert(vo);
 	}
+	//주어진 'MemberVo' 객체로 DB의 회원 정보를 업데이트한다.
 	public int update(MemberVo vo) {
 		return memberDao.update(vo);
 	}
+	//주어진 'pk'에 해당하는 회원 정보를 DB에서 삭제한다.
 	public int delete(String pk) {
 		return memberDao.delete(pk);
 	}
 }
+
+/*
+ 원래는 
+ public List<MemberVo> selectList(){
+ 		List<MemberVo> members = memberDao.selectList();
+		return members
+	}
+*/
