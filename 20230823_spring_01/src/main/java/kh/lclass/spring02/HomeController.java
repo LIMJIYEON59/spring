54646a97055ddb0d2e7bfd21b6fc9e4bf50ce244
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-24 18:30:50 +0900

    Spring 첫 프로젝트

---
+++ 20230823_spring_01/src/main/java/kh/lclass/spring02/HomeController.java
@@ -0,0 +1,47 @@
+package kh.lclass.spring02;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("Welcome home! The client locale is {}dsfsd{}.", locale,233232);
+		logger.warn("warn");
+		logger.error("error");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "bbb/b2";
+	}
+	@GetMapping("/bbb/b2")
+	//@RequestMapping(value = "/bbb/b2", method = RequestMethod.GET)
+	public void b2() {
+	}
+}
