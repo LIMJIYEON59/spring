53a31154919239e8d63f339f5e6dea25e5ccbc7c
LIMJIYEON59 <chayi2110@gmail.com>
2023-09-09 20:21:04 +0900

    복습- 코드에 주석 작성함

--- 20230823_spring_db_01/src/main/java/kh/lclass/db1/board/model/dao/BoardDao.java
+++ 20230823_spring_db_01/src/main/java/kh/lclass/db1/board/model/dao/BoardDao.java
@@ -11,9 +11,12 @@ import kh.lclass.db1.board.model.vo.BoardVo;
 @Repository
 public class BoardDao {
 
-	@Autowired
+	@Autowired //이 어노테이션을 사용하여 Spring에서 자동으로 'SqlSession' 객체를 주입받는다.
 	private SqlSession sqlSession;
 	// throws Exception: 예외처리 오류 방지(500)
+	
+	//selectList는 DB에서 게시판 목록 조회를 하는 메서드이다.
+	//("board.selectList") 이것을 호출하여 MyBatis 매퍼 XML 파일에서 'board.electList'라는 SQL쿼리를 실행한다.
 	public List<BoardVo> selectList() throws Exception {// 이 위로 가서 오류 방지(service->controller 결국 controller에서 최종 처리됨)
 		return sqlSession.selectList("board.selectList");// 여기서 오류 발생하면
 	}
