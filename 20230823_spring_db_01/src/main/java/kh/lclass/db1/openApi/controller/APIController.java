62091b537885c0cab6fb1002acab8c35435b0936
LIMJIYEON59 <chayi2110@gmail.com>
2023-09-27 22:54:21 +0900

    kh시험 api 주석처리(url)

--- 20230823_spring_db_01/src/main/java/kh/lclass/db1/openApi/controller/APIController.java
+++ 20230823_spring_db_01/src/main/java/kh/lclass/db1/openApi/controller/APIController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class APIController {
-	
+	//인코딩된 url은 %3 <-이런식으로 생겼다.
 	private static final String serviceKey = 
 			"ey51rbqUG4nLxm%2FIMXybyk%2FsuSNe95v4aIgn4Ydn0UhcJl91gToKXy7htiiqkWXyGCVxm6xyZpakkO4hk%2FLs3w%3D%3D";
 	
@@ -22,7 +22,9 @@ public class APIController {
 	public String airPollution(String location) throws IOException {
 		String url =  "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty";
 		
+		//그래서 바로 아래처럼 작성가능하다.
 		url += "?serviceKey=" + serviceKey;
+		//디코딩된 url은 이렇게 작성해야한다. location에 담아야한다.
 		url += "&sidoName=" + URLEncoder.encode(location, "UTF-8"); 
 		url += "&returnType=json";
 		
