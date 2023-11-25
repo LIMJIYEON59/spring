8193ae13b76034219674ae7763fb85322366db4f
LIMJIYEON59 <chayi2110@gmail.com>
2023-11-26 00:17:09 +0900

    코테 7번 문제
    1-10,000까지의 수 중 3,6,9의 개수를 구하는 코드

--- codingtest/src/codingtest/Test07.java
+++ codingtest/src/codingtest/Test07.java
@@ -1,5 +1,19 @@
 package codingtest;
 
+// 1-10,000까지의 수 중 3, 6, 9의 개수를 구하는 코드
 public class Test07 {
-
+    public static void main(String[] args) {
+        int nCount = 0;
+        for (int i = 1; i <= 10000; i++) {
+            int n = i;
+            while (n > 0) {
+                int m = n % 10;
+                if (m == 3 || m == 6 || m == 9) {
+                    nCount++;
+                }
+                n = n / 10;
+            }
+        }
+        System.out.printf("3,6,9 개수 :%d \n", nCount);
+    }
 }
