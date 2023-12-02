f0c4169c55b397826cb655163b629e65fdcacef3
LIMJIYEON59 <chayi2110@gmail.com>
2023-12-02 18:37:31 +0900

    09번 문제
    별찍기

---
+++ codingtest/src/codingtest/Test09.java
@@ -0,0 +1,13 @@
+package codingtest;
+
+public class Test09 {
+	public static void main(String[] args) {
+		for(int i=0; i < 5; i++) {
+			for(int j=0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
